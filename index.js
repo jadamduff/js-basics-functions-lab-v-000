ee25da4090b9d925fbad2b53d2648316975bc9d4
Adam Duff <j.adam.duff@gmail.com>
2018-10-22 23:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  switch (block) {
+    case >= 42:
+      return block - 42;
+      break;
+
+    default:
+      return 42 - block;
+      break;
+  }
+}
